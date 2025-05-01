e539a77531b1a2de6b63d914af9be5d85b390cd2
qzyuebing <54806519+qzyuebing@users.noreply.github.com>
2025-05-01 09:33:07 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -21,3 +21,4 @@ sed -i '$a src-git mmdvm https://github.com/qzyuebing/mmdvm-openwrt' feeds.conf.
 #sed -i '$a src-git mmdvm https://github.com/lazywalker/mmdvm-openwrt' feeds.conf.default
 sed -i '$a src-git devtools https://github.com/lazywalker/devtools-feeds' feeds.conf.default
 sed -i '$a src-git NueXini https://github.com/qzyuebing/NueXini_Packages' feeds.conf.default
+sed -i '$a src-git modemband https://github.com/qzyuebing/luci-app-modemband' feeds.conf.default
